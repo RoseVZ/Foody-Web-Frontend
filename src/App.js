89eb58fc2771f25ec733e3a8debde007d28187e8
pratheethadiga <pratheethadiga1@gmail.com>
2022-12-05 23:30:14 +0530

    Creating Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <box>
+        <Grid container columnSpacing={0} rowSpacing={0} backgroundColor='red'>
+          <Grid item xs={6} backgroundColor='white' rowSpacing={10}>
+            <p><img src='https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/611e17ebf252adff5480aef3_DMSlDQUuTZetljlTjKw6NfmtfDbOlaS08FUI42EQJYGrP0sb_Mnhck8S3KmhI9PoQkEsFAqFf21M13WaZ089jerVNTNSz9rTNKfDsareelasXbJ9Z9KkTfVzAZOIB8V0MBwWvavy.jpeg' width='100px' height='50px' alt='30'></img> <a href='https://www.w3schools.com'>Sign-Up</a> <a href='www.w3schools.com'>Login</a></p>
+            <div class="search">
+            
+            <p id="abc"></p>
+        </div>
+            <p>Random delivery locations</p> 
+          </Grid>
+          <Grid item xs={6} rowSpacing={10}>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.WDZIWyYhBONdPWmkoF07NQHaEo%26pid%3DApi&f=1&ipt=15051794bede57b59c86ad5e81c97a0f00313c1fd8d6e11b9c8dab8868aab95b&ipo=images" alt = "some random food pic" width='100%' height='100%'></img>
+          </Grid>
+        </Grid>
+      </box>
+      <box>
+       <Grid container columnSpacing={0} rowSpacing={0} backgroundColor="brown">
+       <Grid item xs={4}>
+        <p>Give us Money</p>
+       </Grid> 
+       <Grid item xs={4}>
+        <p>Give us your hard earned money</p>
+       </Grid>
+       <Grid item xs={4}>
+        <p>Give us more money</p>
+       </Grid>
+       </Grid>
+      </box>
+      <box>
+        <Grid container columnSpacing={0} rowSpacing={0} backgroundColor='black' color='white'>
+        <Grid item xs={4}>Company</Grid>
+        <Grid item xs={4}>Contact Us</Grid>
+        <Grid item xs={4}>Legal</Grid>
+        </Grid>
+      </box>
     </div>
   );
 }
 
-export default App;
---
+++ src/Components/FirstPage/SearchBar.js
@@ -0,0 +1,9 @@
+
+<div>
+    <form action="#">
+                <input type="text"
+                    placeholder=" Search Courses"
+                    name="search" />
+                <button>Search</button>
+    </form>
+</div>
