b63c15eda25fb32099bcda7ebb2c3630ec0c7621
pratheethadiga <pratheethadiga1@gmail.com>
2022-12-05 23:42:18 +0530

    money

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ export default function App() {
           <Grid item xs={6} backgroundColor='white' rowSpacing={10}>
             <p><img src='https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/611e17ebf252adff5480aef3_DMSlDQUuTZetljlTjKw6NfmtfDbOlaS08FUI42EQJYGrP0sb_Mnhck8S3KmhI9PoQkEsFAqFf21M13WaZ089jerVNTNSz9rTNKfDsareelasXbJ9Z9KkTfVzAZOIB8V0MBwWvavy.jpeg' width='100px' height='50px' alt='30'></img> <a href='https://www.w3schools.com'>Sign-Up</a> <a href='www.w3schools.com'>Login</a></p>
             <div class="search">
-            
-            <p id="abc"></p>
+              {/* <SearchBar\> */}
         </div>
             <p>Random delivery locations</p> 
           </Grid>
@@ -20,17 +19,7 @@ export default function App() {
         </Grid>
       </box>
       <box>
-       <Grid container columnSpacing={0} rowSpacing={0} backgroundColor="brown">
-       <Grid item xs={4}>
-        <p>Give us Money</p>
-       </Grid> 
-       <Grid item xs={4}>
-        <p>Give us your hard earned money</p>
-       </Grid>
-       <Grid item xs={4}>
-        <p>Give us more money</p>
-       </Grid>
-       </Grid>
+        {/* <Money\> */}
       </box>
       <box>
         {/* <Footer \> */}
