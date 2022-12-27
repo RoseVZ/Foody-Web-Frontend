442c2b01e76714cfb6e739ed13618f3e1c1eecbe
pratheethadiga <pratheethadiga1@gmail.com>
2022-12-27 08:19:17 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
-// import Checkout from './Components/Pages/CheckOut';
+import Checkout from './Components/Pages/CheckOut';
 import FirstPage from './Components/Pages/FirstPage';
-// import CustomerPage from './Components/Pages/CustomerPage';
+import CustomerPage from './Components/Pages/CustomerPage';
 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<FirstPage />}>
-        <Route index element={<FirstPage />} />
-        {/* <Route path="/signup" element={<SignUp />} /> */}
-        {/* <Route path="contact" element={<Contact />} /> */}
+        <Route index element={<Checkout />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="customerpage" element={<CustomerPage />} />
         {/* <Route path="*" element={<NoPage />} /> */}
       </Route>
     </Routes>
